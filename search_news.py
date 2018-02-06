3c01d9fa24ce2b4185d8b7ea2705ed0b8bbd7efc
search_news <35289556+wangfpp@users.noreply.github.com>
2018-02-06 16:50:03 +0800

    change file
    更新主程序,获取更多的新闻信息

--- search_news.py
+++ search_news.py
@@ -2,9 +2,9 @@
 # @Author: wang
 # @Date:   2018-01-17 10:15:25
 # @Last Modified by:   wangfpp
-# @Last Modified time: 2018-01-18 20:24:58
-import requests
-from bs4 import BeautifulSoup
+# @Last Modified time: 2018-02-05 15:12:00
+import requests#接口请求模块
+from bs4 import BeautifulSoup#网页解析模块
 import os
 import re
 
@@ -13,49 +13,62 @@ class ClassName(object):
     def __init__(self,base_url):#Class类初始化函数  base_url为解读的新闻网页的主页
         self.base_url = base_url
         pass
-    def get_href(self):#获取 网页的a标签的 href  过滤后 进行解析其新闻文本
-        # try:
-        self.alink = alink = []
-        req = requests.get(self.base_url)
-        req.status_code == 200
-        print ('\033[0;32;47m 解析正常进行\033[0m')
-        req_content = req.content
-        soup = BeautifulSoup(req_content, 'html.parser')
-        a_tag = soup.find_all('a')
-        for href in a_tag:
-            if href.get('href'):
-                alink.append(href.get('href'))
-        self.filter_a_href()#对获取的a标签进行过滤
-        for url in self.alink:
-            file_name = (re.sub(self.base_url, '', os.path.splitext(url)[0], 0) + "{0}").replace('/','_').format('.txt')
-            if self.is_have_file(file_name):
-                print ('正在提取:{0}的文字').format(url)
-                self.get_text(url)
-        # except:
-        #     print ('\033[7;31;47m 网页解析出错 \033[0m')#https://www.cnblogs.com/ping-y/p/5897018.html
-        #     #print color ('\033[显示方式;字体颜色;背景颜色m　print text　\033[0m')
+    def classify_news(self,url):
+        self.classify_a = []
+        req = requests.get(url)
+        if req.status_code == 200:
+            req_content = req.content
+            soup = BeautifulSoup(req_content, 'html.parser')
+            classify_a_tag = soup.find_all('ul', class_ = 'nav_navcon')
+            for a in classify_a_tag[0].find_all('a'):
+                self.classify_a.append(a.get('href')) 
+            self.filter_a_href(self.classify_a) 
+        else:
+            pass
+        for i in self.classify_a:
+            self.get_href(i)
+    def get_href(self,url):#获取 网页的a标签的 href  过滤后 进行解析其新闻文本
+        try:
+            self.alink = alink = []
+            req = requests.get(url)
+            print ('\033[0;32;47m 解析正常进行\033[0m')
+            req_content = req.content
+            soup = BeautifulSoup(req_content, 'html.parser')
+            a_tag = soup.find_all('a')
+            for href in a_tag:
+                if href.get('href'):
+                    alink.append(href.get('href'))
+            self.filter_a_href(self.alink)#对获取的a标签进行过滤
+            for uri in self.alink:
+                file_name = (re.sub(self.base_url, '', os.path.splitext(uri)[0], 0) + "{0}").replace('/','_').format('.txt')
+                if self.is_have_file(file_name):
+                    print ('正在提取:{0}的文字').format(uri)
+                    self.get_text(uri)
+        except:
+            print ('\033[7;31;47m 网页解析出错 \033[0m')#https://www.cnblogs.com/ping-y/p/5897018.html
+            #print color ('\033[显示方式;字体颜色;背景颜色m　print text　\033[0m')
 
     def get_text (self,url):#获取新闻网页的新闻内容
         file_name = (re.sub(self.base_url, '', os.path.splitext(url)[0], 0) + "{0}").replace('/','_').format('.txt')
-        #try:
-        req = requests.get(url)#, timeout=3
-        #print ('\033[0;32m 正在获取网页　\033[0m {0} {1}').format(req.status_code,url) 
-        
-        req.encoding = 'GB2312' 
-        html = req.text
-        #try:   
-        soup = BeautifulSoup(html, 'html.parser')
-        content = soup.find_all('div', class_ = 'left_zw')
-        for item in content:
-            for txt_contene in item.contents:
-                if txt_contene.string and len(txt_contene.string) > 5:
-                    #print url,txt_contene.string
-                    print ('保存新闻内容到:{0}').format(file_name)
-                    self.save_text(txt_contene.string.encode('utf-8'),file_name)
-        #     except:
-        #         print ('\033[0;31m 网页解析错误\033[0m {0}').format(url)
-        # except:
-        #     print ('\033[0;31m 网页获取错误\033[0m {0}').format(url)
+        try:
+            req = requests.get(url,timeout=10)#, timeout=3
+            #print ('\033[0;32m 正在获取网页　\033[0m {0} {1}').format(req.status_code,url) 
+
+            req.encoding = 'GB2312' 
+            html = req.text
+            try:   
+                soup = BeautifulSoup(html, 'html.parser')
+                content = soup.find_all('div', class_ = 'left_zw')
+                for item in content:
+                    for txt_contene in item.contents:
+                        if txt_contene.string and len(txt_contene.string) > 5:
+                            #print url,txt_contene.string
+                            print ('保存新闻内容到:{0}').format(file_name)
+                            self.save_text(txt_contene.string.encode('utf-8'),file_name)
+            except:
+                print ('\033[0;31m 网页解析错误\033[0m {0}').format(url)
+        except:
+            print ('\033[0;31m 网页获取错误\033[0m {0}').format(url)
 
     def save_text(self,txt,filename):#保存新闻内容到txt文件中
         f = open('./txt/' + filename, 'a')
@@ -69,20 +82,21 @@ class ClassName(object):
         else:
             return True
 
-    def filter_a_href (self):#过滤a标签
-        for a in  range(len(self.alink)):
-            if self.alink[a].find('//', 0, 2) != -1:
-                self.alink[a] = 'http:' + self.alink[a]
-            if self.alink[a].find('/', 0, 1) != -1:
-                self.alink[a] = 'http://www.chinanews.com' + self.alink[a]
-        for item in range(len(self.alink))[::-1]:
-            if not 'http://www.chinanews.com' in self.alink[item]:
-                del self.alink[item] 
+    def filter_a_href (self,arr):#过滤a标签
+        for a in  range(len(arr)):
+            if arr[a].find('//', 0, 2) != -1:
+                arr[a] = 'http:' + arr[a]
+            if arr[a].find('/', 0, 1) != -1:
+                arr[a] = 'http://www.chinanews.com' + arr[a]
+        for item in range(len(arr))[::-1]:
+            if not 'http://www.chinanews.com' in arr[item] or 'shipin' in arr[item]:
+                del arr[item] 
+
 
 if __name__ == '__main__':
     base_url = 'http://www.chinanews.com/'
     a = ClassName(base_url)
-    a.get_href()
+    a.classify_news(base_url)
         
   			
   		
